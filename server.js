5203784a388ac3dfa233b976f5cb49f7c51bc1a8
Jenna Chestnut <jennabot5000@gmail.com>
2020-11-24 14:01:07 -0600

    planning phase

--- server.js
+++ server.js
@@ -3,15 +3,30 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const movies = require('./movies-data-small');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 
-app.get('/', (req, res) => {
+const handleAuth = (req, res, next) => {
+    // verifies auth token
+}
+
+const searchByTerm = (store, searchType, searchTerm) => {
+    // handles search type and uses term to return filtered array
+}
+
+const searchByVote = (store, avgVote) => {
+    // handles search # and uses number to return filtered array
+}
+
+const curateResponse = (req, res) => {
     res.send('Hellooooo, Express!');
-});
+}
+
+app.get('/movie', curateResponse);
 
 app.listen(8000, () => {
     console.log('Express server is listening on port 8000!');
