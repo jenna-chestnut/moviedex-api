a8a6e0d83019336b2411eb41aeb053a41218107b
Jenna Chestnut <jennabot5000@gmail.com>
2020-12-01 19:14:45 -0600

    preparing for deployment

--- server.js
+++ server.js
@@ -6,89 +6,95 @@ const helmet = require('helmet');
 const movies = require('./movies-data-small');
 
 const handleAuth = (req, res, next) => {
-    // verifies auth token
-    const apiToken = process.env.API_TOKEN;
-    const authToken = req.get('Authorization');
+  // verifies auth token
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401)
-            .json({ error: 'Unauthorized Request' });
-    }
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({
+      error: 'Unauthorized Request'
+    });
+  }
 
-    next();
-}
+  next();
+};
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 app.use(handleAuth);
 
 const searchByTerm = (store, searchType, searchTerm) => {
-    // handles search type and uses term to return filtered array
-    searchTerm = searchTerm.toLowerCase();
+  // handles search type and uses term to return filtered array
+  searchTerm = searchTerm.toLowerCase();
 
-    const filtered = store
-        .filter(mov => {
-            const type = mov[searchType].toLowerCase();
-            return type.includes(searchTerm);
-        });
+  const filtered = store
+    .filter(mov => {
+      const type = mov[searchType].toLowerCase();
+      return type.includes(searchTerm);
+    });
 
-    return filtered;
-}
+  return filtered;
+};
 
 const searchByVote = (store, avgVote) => {
-    // handles search # and uses number to return filtered array
-    const filtered = store
-        .filter(mov => mov.avg_vote >= avgVote);
+  // handles search # and uses number to return filtered array
+  const filtered = store
+    .filter(mov => mov.avg_vote >= avgVote);
 
-    return filtered;
-}
+  return filtered;
+};
 
-const curateResponse = (req, res) => {
-    // start with the whole dang store
-    let response = movies;
+const curateResponse = (req, res, err) => {
+  // start with the whole dang store
+  let response = movies;
 
-    // deconstruct queries
-    const { genre, country, avg_vote } = req.query;
+  // deconstruct queries
+  const { genre, country, avg_vote } = req.query;
 
-    // do not accept lack of query 
-    if (!genre && !country && !avg_vote) {
-        res.status(400).json({ error: 'Please provide at least one query!' });
-        
+  //does genre query exist? if so, run a search, otherwise do nothing
+  if (genre) response = searchByTerm(response, 'genre', genre);
+
+  //does country query exist? if so, run a search, otherwise do nothing
+  if (country) response = searchByTerm(response, 'country', country);
+
+  //does avg vote query exist? if so, attempt to run a search, otherwise do nothing
+  const vote = parseInt(avg_vote);
+  if (avg_vote) {
+    // only run a search if number is valid - throw error otherwise
+    if (!Number.isNaN(vote)) {
+      response = searchByVote(response, vote);
     } else {
-        //does genre query exist? if so, run a search, otherwise do nothing
-        genre ?
-            response = searchByTerm(response, 'genre', genre)
-            : response;
-
-        //does country query exist? if so, run a search, otherwise do nothing
-        country ?
-            response = searchByTerm(response, 'country', country)
-            : response;
-
-        //does avg vote query exist? if so, attempt to run a search, otherwise do nothing
-        const vote = parseInt(avg_vote);
-        avg_vote ?
-            // only run a search if number is valid - throw error otherwise
-            !Number.isNaN(vote) ?
-                response = searchByVote(response, vote)
-                : 
-                res.status(400).json({ error: 'Average vote must be a number!' })
-            : response;
-
-        //if nothing matches a search, send a message instead of empty array
-        response.length === 0 ?
-            response = 'Sorry! No results found, please try again.'
-            : response;
-
-        res.status(200).json(response);
+      err('Average vote must be a number!');
+      return;
     }
-}
+  }
+
+  //if nothing matches a search, send a message instead of empty array
+  if (response.length === 0) {
+    response = 'Sorry! No results found, please try again.';
+  }
+
+  res.status(200).json(response);
+};
 
 app.get('/movie', curateResponse);
 
-app.listen(8000, () => {
-    console.log('Express server is listening on port 8000!');
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+  
 
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+});
